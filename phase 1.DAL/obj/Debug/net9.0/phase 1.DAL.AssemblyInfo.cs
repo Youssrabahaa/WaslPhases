c4ef599bc0e5b28801c41f661752c788901a68fb
Abdo <abdo@gmail.com>
2026-06-23 23:39:30 +0300

    Update binaries and assembly information for phase 1.BLL and phase 1.DAL
    - Updated phase 1.BLL.dll and phase 1.BLL.pdb with new builds.
    - Updated phase 1.DAL.dll and phase 1.DAL.pdb with new builds.
    - Modified AssemblyInformationalVersion in phase 1.BLL and phase 1.DAL to reflect new version.
    - Updated ProjectDir paths in editorconfig files for both projects to use lowercase drive letters.
    - Updated sourcelink.json files for both projects to point to the new commit hashes.
    - Updated assets.cache files for both projects to reflect changes in dependencies.

--- phase 1.DAL/obj/Debug/net9.0/phase 1.DAL.AssemblyInfo.cs
+++ phase 1.DAL/obj/Debug/net9.0/phase 1.DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("phase 1.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd9888ecad73eb19a2cf6d8585361eca65469990")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92b5dc49e8c2694eeaecf11f10827004003d3f3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("phase 1.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("phase 1.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
